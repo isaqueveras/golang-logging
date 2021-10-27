89567f1a54b1dff80a6ec2c70a8fc968f0fe90d5
isaqueveras <isaque.verasccb@gmail.com>
2021-10-27 09:05:08 -0300

    logging on golang

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func init() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.LstdFlags|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.LstdFlags|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.LstdFlags|log.Lshortfile)
+}
+
+func main() {
+	InfoLogger.Println("This is some info")
+	WarningLogger.Println("This is some warning")
+	ErrorLogger.Println("This is some error")
+
+	log.Println("This my log message")
+}
